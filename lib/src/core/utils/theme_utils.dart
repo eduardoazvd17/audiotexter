1111ea33f9db3a992e15b89adf4b336ec9adc535
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-02-05 11:06:49 -0300

    Fixing theme primaryColor

--- lib/src/core/utils/theme_utils.dart
+++ lib/src/core/utils/theme_utils.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ThemeUtils {
+  static const primaryColor = Colors.blueGrey;
   static get borderColor => Colors.grey.shade100;
 
   static ThemeData get lightTheme => ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blueGrey,
+          seedColor: primaryColor,
           brightness: Brightness.light,
+          primary: primaryColor,
         ),
+        primaryColor: primaryColor,
         useMaterial3: true,
       );
 
   static ThemeData get darkTheme => ThemeData(
         colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blueGrey,
+          seedColor: primaryColor,
           brightness: Brightness.dark,
+          primary: primaryColor,
         ),
+        primaryColor: primaryColor,
         useMaterial3: true,
       );
 }
