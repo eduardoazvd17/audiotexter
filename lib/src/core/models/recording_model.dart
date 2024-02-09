869bada4801e311599e4fcea1a678b094ec15197
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-02-09 13:51:29 -0300

    Removing import

--- lib/src/core/models/recording_model.dart
+++ lib/src/core/models/recording_model.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:io';
-import 'dart:ui';
 
 import 'package:equatable/equatable.dart';
 import 'package:intl/intl.dart';
