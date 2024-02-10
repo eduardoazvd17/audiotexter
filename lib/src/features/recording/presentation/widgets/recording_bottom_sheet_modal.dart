8734cbc6ba5805926be14e07310f48b83d925d13
Eduardo Azevedo <eduardoazvd17@gmail.com>
2024-02-10 13:44:56 -0300

    Adding wave animation to recording modal

--- lib/src/features/recording/presentation/widgets/recording_bottom_sheet_modal.dart
+++ lib/src/features/recording/presentation/widgets/recording_bottom_sheet_modal.dart
@@ -6,6 +6,7 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 
 import 'package:audiotexter/src/features/l10n/l10n.dart';
 import 'package:audiotexter/src/features/recording/presentation/controllers/recording_controller.dart';
+import 'package:lottie/lottie.dart';
 
 class RecordingBottomSheetModal extends StatelessWidget {
   final RecordingController controller;
@@ -36,91 +37,109 @@ class RecordingBottomSheetModal extends StatelessWidget {
               );
             }
 
-            return Padding(
-              padding: const EdgeInsets.all(25),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            return Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Text(
-                        controller.isPaused
-                            ? AppLocalizations.of(context)!.paused
-                            : AppLocalizations.of(context)!.recording,
-                        style: Theme.of(context).textTheme.titleMedium,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            controller.isPaused
+                                ? AppLocalizations.of(context)!.paused
+                                : AppLocalizations.of(context)!.recording,
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                          Text(
+                            controller.timerString,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
                       ),
-                      Text(
-                        controller.timerString,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      const SizedBox(height: 100),
+                      Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: TextFormField(
+                          controller: controller.nameController,
+                          textCapitalization: TextCapitalization.sentences,
+                          textInputAction: TextInputAction.done,
+                          decoration: InputDecoration(
+                            label: Text(AppLocalizations.of(context)!.name),
+                            suffixIcon: const Icon(Icons.audio_file_outlined),
+                            border: const OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            IconButton(
+                              onPressed: () async {
+                                if (controller.isPaused) {
+                                  await controller.resumeRecording();
+                                } else {
+                                  await controller.pauseRecording();
+                                }
+                              },
+                              icon: Icon(
+                                controller.isPaused
+                                    ? Icons.play_arrow
+                                    : Icons.pause,
+                                size: 30,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                controller
+                                    .stopRecording()
+                                    .then((recordingModel) {
+                                  Navigator.of(context).pop(recordingModel);
+                                });
+                              },
+                              icon: Icon(
+                                Icons.save,
+                                size: 40,
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                controller
+                                    .stopRecording()
+                                    .then((recordingModel) {
+                                  if (recordingModel != null) {
+                                    File(recordingModel.path).delete();
+                                  }
+                                  Navigator.of(context).pop(null);
+                                });
+                              },
+                              icon: Icon(
+                                CupertinoIcons.delete,
+                                size: 30,
+                                color: Theme.of(context).colorScheme.error,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(25),
-                    child: TextFormField(
-                      controller: controller.nameController,
-                      textCapitalization: TextCapitalization.sentences,
-                      textInputAction: TextInputAction.done,
-                      decoration: InputDecoration(
-                        label: Text(AppLocalizations.of(context)!.name),
-                        suffixIcon: const Icon(Icons.audio_file_outlined),
-                        border: const OutlineInputBorder(),
-                      ),
-                    ),
+                ),
+                Positioned(
+                  top: -100,
+                  right: 0,
+                  left: 0,
+                  child: IgnorePointer(
+                    ignoring: true,
+                    child: Lottie.asset('assets/animations/audiowave.json'),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        IconButton(
-                          onPressed: () async {
-                            if (controller.isPaused) {
-                              await controller.resumeRecording();
-                            } else {
-                              await controller.pauseRecording();
-                            }
-                          },
-                          icon: Icon(
-                            controller.isPaused
-                                ? Icons.play_arrow
-                                : Icons.pause,
-                            size: 30,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            controller.stopRecording().then((recordingModel) {
-                              Navigator.of(context).pop(recordingModel);
-                            });
-                          },
-                          icon: Icon(
-                            Icons.save,
-                            size: 40,
-                            color: Theme.of(context).colorScheme.secondary,
-                          ),
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            controller.stopRecording().then((recordingModel) {
-                              if (recordingModel != null) {
-                                File(recordingModel.path).delete();
-                              }
-                              Navigator.of(context).pop(null);
-                            });
-                          },
-                          icon: Icon(
-                            CupertinoIcons.delete,
-                            size: 30,
-                            color: Theme.of(context).colorScheme.error,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             );
           },
         ),
